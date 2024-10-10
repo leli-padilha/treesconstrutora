30ac86f79317657d4d957130d974399e529d664d
leli-padilha <wellingson.sk82010@gmail.com>
2024-10-10 20:56:49 -0300

    meta tags

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,19 @@ export const metadata = {
     icon: '/images/logo/logo.ico',
     apple: '/images/logo/logo.png',
   },
+  openGraph: {
+    title: 'Trees Construtora',
+    description: 'Construímos seus investimento e seus sonhos.',
+    url: 'https://treeconstrutora.vercel.app', // URL da página
+    images: [
+      {
+        url: '/image/logo/logo.webp', // URL da imagem
+        width: 673, // Largura da imagem
+        height: 260, // Altura da imagem
+        alt: 'Logo Trees Construtora',
+      },
+    ],
+  }
 };
 
 export default function RootLayout({ children }) {
@@ -16,6 +29,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <meta name="description" content="Constuímos investimenos e seus sonhos." />
+        <meta property="og:title" content="Trees Construtora" />
+        <meta property="og:description" content="Uma descrição para Open Graph." />
+        <meta property="og:image" content="/images/logo/logo.webp" />
         <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
